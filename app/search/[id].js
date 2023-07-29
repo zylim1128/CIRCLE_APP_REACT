510a17c32039dcea20a4b71879654530f37fcefe
zylim1128 <zylim@uw.edu>
2023-07-29 00:18:02 -0700

    Update to Hide API-key

--- app/search/[id].js
+++ app/search/[id].js
@@ -27,7 +27,7 @@ const JobSearch = () => {
                 url: `https://jsearch.p.rapidapi.com/search`,
                 headers: {
                     // TODO: Hide API-key
-                    "X-RapidAPI-Key": '9be06b93bbmsh2a362a74cf1abc5p1071ffjsn472cbd24dbe9',
+                    "X-RapidAPI-Key": '',
                     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
                 },
                 params: {
