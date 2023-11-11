8c2e5b5699afb98a4d34d5daf64b6371ecb79e04
zylim1128 <zylim@uw.edu>
2023-11-11 15:46:56 -0800

    npm install expo-sqlite

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+// Documentation: https://docs.expo.dev/versions/latest/sdk/sqlite/ 
 const { getDefaultConfig } = require('expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push('db');
 
 module.exports = getDefaultConfig(__dirname);
